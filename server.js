3812a6ae9a2c1fd42bde605095633ca78ebe689e
anila-jawed <73032957+hashamuddin1@users.noreply.github.com>
2023-07-12 12:59:15 +0500

    mongoose connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
+const mongoose = require("mongoose");
 
 require("./config/db");
 const userRouter = require("./router/userRoute");
@@ -9,6 +10,14 @@ const subscriptionRouter = require("./router/subscriptionRoute");
 const customerRouter = require("./router/customerRoute");
 const productRouter = require("./router/productRoute");
 const orderRouter = require("./router/orderRoute");
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URL);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +28,8 @@ app.use([
   productRouter,
   orderRouter,
 ]);
-
-app.listen(port, () => {
-  console.log(`Our Server is running at port ${port}`);
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Our Server is running at port ${port}`);
+  });
 });
